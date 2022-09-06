9f83eaa7d6dffead96a3c07afc47440485bf03bc
Mfrozzz <marcosvinibva@gmail.com>
2022-09-05 23:34:14 -0300

    mudanças login e inicio cadastro

--- SisContract/src/app/app-routing.module.ts
+++ SisContract/src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'cadastro',
+    loadChildren: () => import('./pages/cadastro/cadastro.module').then( m => m.CadastroPageModule)
   }
 ];
 
