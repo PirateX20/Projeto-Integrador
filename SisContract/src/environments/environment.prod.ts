3e3c53d3ac07d0e4d996c298eea0036bac61562f
Mfrozzz <marcosvinibva@gmail.com>
2022-09-21 16:41:25 -0300

    inicio services,formcadastro ,termosUso e firebase
    Co-authored-by: PirateX20 <PirateX20@users.noreply.github.com>

--- SisContract/src/environments/environment.prod.ts
+++ SisContract/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCVVIcoaUdqrteCJ3map9DBHg40xE6tcQc",
+    //authDomain: "siscontract-ba6a6.firebaseapp.com",
+    authDomain: "localhost",
+    projectId: "siscontract-ba6a6",
+    storageBucket: "siscontract-ba6a6.appspot.com",
+    messagingSenderId: "508582244110",
+    appId: "1:508582244110:web:a4e6b39387a3a77c31715f"
+  }
 };
